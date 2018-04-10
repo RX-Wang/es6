0ca8e31ff348021ec524418b89d3cc0825a979b8
wangxuquan.byh <wangxuquan.byh@meituan.com>
2018-04-10 17:10:33 +0800

    修改：socketjs 的一些代码

--- test/socketJS-依赖包/index.js
+++ test/socketJS-依赖包/index.js
@@ -1,9 +1,11 @@
 var http = require('http');
 var fs = require('fs');
+var path = require('path');
 var sockjs = require('sockjs');
  
 var echo = sockjs.createServer({ sockjs_url: 'http://cdn.jsdelivr.net/sockjs/1.0.1/sockjs.min.js' });
 echo.on('connection', function(conn) {
+    console.log('客户端建立了链接');
     conn.on('data', function(message) {
       console.log('received client message：', message);
         conn.write(message);
@@ -12,12 +14,10 @@ echo.on('connection', function(conn) {
       console.log('客户端关闭了链接');
     });
 });
- 
+
 var server = http.createServer(function(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  // res.writeHead("Access-Control-Allow-Headers", "X-Requested-With");
-  // res.writeHead("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-  fs.readFile('./index.html', function(err, data) {
+  fs.readFile(path.join(__dirname, 'index.html'), function(err, data) {
     res.writeHead(200, {"Content-Type": "text/html"});
     res.write(data.toString());
     res.end();
