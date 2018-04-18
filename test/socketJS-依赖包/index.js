590bf00785c3b8d448392dd82453e7f0d145b9ca
wangxuquan.byh <wangxuquan.byh@meituan.com>
2018-04-18 19:04:23 +0800

    添加 ws 包做的websocket

--- test/socketJS-依赖包/index.js
+++ test/socketJS-依赖包/index.js
@@ -1,4 +1,4 @@
-var http = require('http');
+/* var http = require('http');
 var fs = require('fs');
 var path = require('path');
 var sockjs = require('sockjs');
@@ -24,4 +24,37 @@ var server = http.createServer(function(req, res) {
   });
 });
 echo.installHandlers(server, {prefix:'/echo'});
+server.listen(9999, '127.0.0.1'); */
+
+/**
+ * ws 创建 websocket 服务端
+ */
+
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+
+var server = http.createServer(function(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  fs.readFile(path.join(__dirname, 'index-ws.html'), function(err, data) {
+    res.writeHead(200, {"Content-Type": "text/html"});
+    res.write(data.toString());
+    res.end();
+  });
+});
 server.listen(9999, '127.0.0.1');
+
+var WebSocketServer = require('ws').Server,
+wss = new WebSocketServer({ port: 8181 });
+wss.on('connection', function (ws) {
+  console.log('client connected');
+  ws.on('message', function (message) {
+    console.log(message);
+  });
+  /* setInterval(
+    function() {
+      ws.send('哈喽，客户端' + (Math.random() * 100000000000).toString(36))
+    },
+    5000
+  ); */
+});
