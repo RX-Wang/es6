b6ef888ab0186c90faa2d912be515eacaf1ac7e1
wangxuquan <wangxuquan@meituan.com>
2018-07-06 15:40:06 +0800

    添加：js获取变量类型；基于缓存的递归 菲波那切数列

---
+++ 学习/JS 获取变量的类型.js
@@ -0,0 +1,20 @@
+var num = 123;
+var str = 'string';
+var bool = true;
+var fun = function() {
+  console.log('function');
+}
+var arr = [1,2,3,4];
+var obj = { name: 'Diana' };
+
+function getType(param) {
+  var type = Object.prototype.toString.call(param);
+  type = type.split(' ')[1].replace(']','');
+  console.log(type);
+}
+getType(num);
+getType(str);
+getType(bool);
+getType(fun);
+getType(arr);
+getType(obj);
