8f930cfae3aa155faf0d9c0887be6b560b2fc497
wangxuquan <wangxuquan@meituan.com>
2018-12-11 20:14:13 +0800

    添加：echarts 可水平滑动的 柱状图

---
+++ view/Vue/echarts/index.js
@@ -0,0 +1,60 @@
+var myChart = echarts.init(document.getElementById('main'));
+var option = {
+  tooltip : {
+      trigger: 'axis'
+  },
+  legend: {
+      data: ['我的酒店', '同行酒店', '平均房价']
+  },
+  grid: {
+      top: '12%',
+      left: '10%',
+      right: '10%',
+      containLabel: true
+  },
+  xAxis: [
+      {
+          type : 'category',
+          data : data.times
+      }
+  ],
+  yAxis: [
+      {
+          type : 'value',
+          name : '入住间夜'
+      }
+  ],
+  dataZoom: [
+      {
+          type: 'slider',
+          show: false,
+          start: 0,
+          end: 50,
+          handleSize: 8
+      },
+      {
+          type: 'inside',
+          start: 9,
+          end: 100
+      },
+  ],
+  series : [
+      {
+          name: '我的酒店',
+          type: 'bar',
+          data: data.myPoiDatas
+      },
+      {
+          name: '同行酒店',
+          type: 'bar',
+          data: data.othersPoiDatas
+      },
+      {
+          name:'平均房价',
+          type:'line',
+          data: data.averageData
+      }
+  ]
+};
+
+myChart.setOption(option);
